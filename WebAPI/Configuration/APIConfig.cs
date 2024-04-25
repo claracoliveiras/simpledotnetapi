38030bf0594cc7538bfd35075626ddddc16c6643
Clara Campos <claracoliveiras@gmail.com>
2024-04-25 09:06:13 -0300

    push
    everything was wrong lol

---
+++ WebAPI/Configuration/APIConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Configuration
+{
+    public static class APIConfig
+    {
+        public static WebApplicationBuilder AddAPIConfig (this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            return builder;
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -6,80 +6,14 @@ using Microsoft.OpenApi.Models;
 using System.Text;
 using WebAPI.Data;
 using WebAPI.Models;
+using WebAPI.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Insira o token JWT desta maneira: Bearer {seu token}",
-        Name = "Authorization",
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-}
-    
-);
-
-builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<APIDBContext>(
-    opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddRoles<IdentityRole>().AddEntityFrameworkStores<APIDBContext>();
-
-var JwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
-builder.Services.Configure<JWTSettings>(JwtSettingsSection);
-
-var jwtSettings = JwtSettingsSection.Get<JWTSettings>();
-var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = true;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = jwtSettings.Audiencia,
-        ValidIssuer = jwtSettings.Audiencia,
-    };
-});
+builder.AddAPIConfig().AddCorsConfig().AddSwaggerConfig().AddDBConfig().AddIdentityConfig();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
